8a0ce48a171ceb6632cfd6ede8d00e53390f9327
jamel <jamel@JAMELS_LAPTOP>
2026-02-13 19:42:40 -0800

    Add project files.

---
+++ LifeBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GameOfLife
+{
+    public class LifeBoard
+    {
+        bool[,] Grid = new bool[,] { { true, false }, { false, true } };
+        
+        public int Rows = 10;
+        public int Columns = 10;
+
+        public LifeBoard()
+        {
+            Grid = new bool[Rows, Columns];
+        }
+
+        public void PrintBoard()
+        {
+            for (int b = 1; b < Rows; b++)
+            {
+                for (int e = 1; e < Columns; e++)
+                {
+                    Console.Write(Grid[b, e]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ClearBoard()
+        {
+
+            Console.Clear();
+        }
+
+        public void CountBoard()
+        {
+            int totalCount = 0 + 1;
+            totalCount++;
+
+            foreach (var rows in Grid)
+          foreach (var col in Grid)
+
+           
+            Console.WriteLine(totalCount);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LifeBoard printBoard = new LifeBoard();
+
+            printBoard.PrintBoard();
+
+            Console.ReadLine();
+            
+            LifeBoard countBoard = new LifeBoard();
+
+            countBoard.CountBoard();
+
+            Console.ReadLine();
+
+            LifeBoard clearBoard = new LifeBoard();
+
+            clearBoard.ClearBoard();
+        }
+    }
+}
